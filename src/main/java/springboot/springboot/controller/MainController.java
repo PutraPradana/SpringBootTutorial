b93809940cbfda4868dc44b095d111aebcc86329
PutraPradana <ppradana92@gmail.com>
2019-07-19 09:10:47 +0700

    mapping

--- src/main/java/springboot/springboot/controller/MainController.java
+++ src/main/java/springboot/springboot/controller/MainController.java
@@ -5,8 +5,11 @@
  */
 package springboot.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import springboot.springboot.service.CountryService;
 
 /**
  *
@@ -15,8 +18,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
+    @Autowired
+    private CountryService countryService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        String id = "2";
+        model.addAttribute("dataCountry", countryService.findAll());
         return "index";
     }
 }
